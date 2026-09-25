0097fda970409e99d703f555f861f74af7b098a1
Ilya Filippov <themorgot@yandex.ru>
2026-09-26 00:03:54 +0300

    Added validation for stdio transport

--- tstdio.c
+++ tstdio.c
@@ -1,8 +1,17 @@
+#include <stdio.h>
 #include "tstdio.h"
 
 int transport_create_stdio(TransportStdio *s, TransportCommon *c, Options *o)
 {
     (void)s; (void)o;
+    if (o->reuseaddr || o->so_rcvbuf > 0 || o->so_sndbuf > 0 ||
+        o->server || o->port > 0)
+    {
+        fprintf(stderr, "Error creating stdio transport: invalid options for stdio set\n");
+        return -1;
+    }
+
+
     c->fdin = 0;
     c->fdout = 1;
     return 0;
